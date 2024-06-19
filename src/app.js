0a78b6a9220dc7f2380ead8c0fb3644cfaa6d63a
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-06-19 18:52:28 +0000

    Se corrigio la forma de como estaba organizado el for

--- src/app.js
+++ src/app.js
@@ -9,17 +9,17 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };*/
-let handle="www.";
+let handle = "www.";
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let dominio=[".com",".gob",".es",".ar",".net"]
-for(let i of pronoun){
- for(let j of adj){
-   for(let k of noun){
-      for(let m of dominio){
-      console.log(handle+i+j+k+m);
-      }    
-   }
- }
+let dominio = [".com", ".gob", ".es", ".ar", ".net"];
+for (let i of pronoun) {
+  for (let j of adj) {
+    for (let k of noun) {
+      for (let m of dominio) {
+        console.log(handle + i + j + k + m);
+      }
+    }
+  }
 }
