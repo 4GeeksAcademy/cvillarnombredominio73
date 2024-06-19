cb8a2b8e1ac207f4aee05beb3f2cf109da40f791
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-06-19 18:48:02 +0000

    Creacion de generador de dominios con ford

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
@@ -8,4 +8,18 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-};
+};*/
+let handle="www.";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dominio=[".com",".gob",".es",".ar",".net"]
+for(let i of pronoun){
+ for(let j of adj){
+   for(let k of noun){
+      for(let m of dominio){
+      console.log(handle+i+j+k+m);
+      }    
+   }
+ }
+}
